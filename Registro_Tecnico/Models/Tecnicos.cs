fadc821eb030bcf0200fb55bce9e3f1bc9f76502
DjMarte <alphat098@gmail.com>
2024-05-07 17:06:42 -0400

    Cree agregué la carpeta Models y su modelo

---
+++ Registro_Tecnico/Models/Tecnicos.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Registro_Tecnico.Models;
+
+public class Tecnicos
+{
+    [Key]
+    public int TecnicoId { get; set; }
+
+    [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "No se permiten caracteres especiales")]
+    [Required(ErrorMessage = "Nombre obligatorio")]
+    public string? Nombres { get; set; }
+
+    [Required(ErrorMessage = "Sueldo obligtorio")]
+    public decimal SueldoHota { get; set; } 
+}
