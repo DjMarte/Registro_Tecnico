f5675d93bc648ae0036711b27c526ee8d7839bd5
DjMarte <alphat098@gmail.com>
2024-05-08 18:08:18 -0400

    Le agregué un rango al sueldo y corregí nuevamenta la expresión regula del Nombre

--- Registro_Tecnico/Models/Tecnicos.cs
+++ Registro_Tecnico/Models/Tecnicos.cs
@@ -7,10 +7,11 @@ public class Tecnicos
     [Key]
     public int TecnicoId { get; set; }
 
-    [RegularExpression(@"^[a-zA-Z]\s[a-zA-Z]+$", ErrorMessage = "No se permiten caracteres especiales")]
+    [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "No se permiten caracteres especiales")]
     [Required(ErrorMessage = "Nombre obligatorio")]
     public string? Nombres { get; set; }
 
+    [Range(0.01, float.MaxValue, ErrorMessage ="Ingrese un valor mayor a 0")] 
     [Required(ErrorMessage = "Sueldo obligtorio")]
     public decimal SueldoHora { get; set; } 
 }
